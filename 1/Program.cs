8f2daae6defbc1773e8a76b9a7babe094bd6248d
nestaraus <nestaraus@mail.pu>
2023-10-17 21:06:45 +0300

    zadacha 1

---
+++ 1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] =new Random().Next(-10,11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix) 
+{
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int temp = 0;
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    temp = matrix[0,j];  
+    matrix[0,j]=matrix[matrix.GetLength(0)-1,j];
+    matrix[matrix.GetLength(0)-1,j] = temp;
+
+}
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x=> int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+PrintMatrix(matrix);
